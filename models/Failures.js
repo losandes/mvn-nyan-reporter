a922f79aa7795a10a5b011c4c7e27ce5dc80f8bb
losandes <andes.collab@gmail.com>
2017-03-06 11:50:11 -0500

    fixes line 1 length of letter i, and expected-actual output for failures

--- models/Failures.js
+++ models/Failures.js
@@ -40,9 +40,9 @@ function Failures (input) {
                 failure.comparison.indexOf('but was') > -1
             ) {
                 tmp = failure.comparison.split('expected:')[1];
-                failure.expected = tmp.substring(2, tmp.indexOf(']>'));
+                failure.expected = tmp.substring(1, tmp.indexOf('>'));
                 tmp = tmp.split('but was:')[1];
-                failure.actual = tmp.substring(2, tmp.indexOf(']>'));
+                failure.actual = tmp.substring(1, tmp.indexOf('>'));
             } else {
                 failure.comparison += '\n';
             }
